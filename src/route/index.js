f5a5ba6c1bec390ae1eb57a0fee5e651c02d4ccc
sattva2020 <ruslan.griban@gmail.com>
2023-12-02 17:53:25 +0200

    created user crud

--- src/route/index.js
+++ src/route/index.js
@@ -4,22 +4,139 @@ const express = require('express')
 const router = express.Router()
 
 // ================================================================
+class User {
+  static #list = []
+
+  constructor(email, login, password) {
+    this.email = email
+    this.login = login
+    this.password = password
+    this.id = new Date().getTime()
+  }
+
+  verifyPassword = (password) => this.password === password
+
+  static add = (user) => {
+    this.#list.push(user)
+  }
+
+  static getList = () => this.#list
+  
+
+  static getById = (id) => 
+  this.#list.find((user) => user.id === id)  
+
+  static deleteById = (id) => {
+   const index = this.#list.findIndex(
+    (user) => user.id === id,
+    ) 
+    if (index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static updateById = (id, data) => {
+    const user = this.getById(id)
+
+    if (user) {
+      this.update(user, data) 
+
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static update = (user, {email}) => {
+    if (email) {
+        user.email = email
+      }   
+  }
+
+}
+// ================================================================
 
 // router.get Створює нам один ентпоїнт
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
+  const list = User.getList()
 
   // ↙️ cюди вводимо назву файлу з сontainer
   res.render('index', {
     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
     style: 'index',
+
+    data: {
+      users: {
+      list: list,
+      isEmpty: list.length === 0,
+      }
+    },
+
   })
   // ↑↑ сюди вводимо JSON дані
 })
 
 // ================================================================
+
+router.post('/user-create', function (req, res) {
+  // res.render генерує нам HTML сторінку
+  const {email, login, password} = req.body
+
+  const user = new User(email, login, password);
+
+  User.add(user)
+
+  console.log(User.getList())
+
+  res.render('success-info', {
+    style: 'success-info',
+    info: 'Користувач створенний',
+  })
+})
+
+// ================================================================
+
+router.get('/user-delete', function (req, res) {
+  // res.render генерує нам HTML сторінку
+  const { id } = req.query
+
+  User.deleteById(Number(id))
+
+  res.render('success-info', {
+    style: 'success-info',
+    info: 'Користувач видаленний',
+  })
+})
+
+// ================================================================
+
+router.post('/user-update', function (req, res) {
+  // res.render генерує нам HTML сторінку
+  const {email, password, id} = req.body
+
+  let result = false
+
+  const user = User.getById(Number(id))
+
+  if (user.verifyPassword(password)) {
+    User.update(user, {email})
+    result = true
+  }
+
+
+  res.render('success-info', {
+    style: 'success-info',
+    info: result
+      ? 'Email пошта оновлена'
+      : 'Сталася помилка',
+  })
+})
 
 // Підключаємо роутер до бек-енду
 module.exports = router
