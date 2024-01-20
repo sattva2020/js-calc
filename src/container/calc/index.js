063ab0e341fe090c2c6de6c6d6fdc8ee1142b14f
sattva2020 <ruslan.griban@gmail.com>
2024-01-20 20:14:05 +0200

    js-calc_2

--- src/container/calc/index.js
+++ src/container/calc/index.js
@@ -9,7 +9,6 @@ class Calc {
     }
 
   static #output = () => {
-    // this.#save()
     window.output.innerHTML = this.#value
   }
 
@@ -18,50 +17,5 @@ class Calc {
     this.#output()
 
     }
-
-  //   if (isNaN(this.#value[this.#value.length - 1])) {
-  //     return null
-  //   }
-
-  //   
-  //   
-  //   this.#isDot = true
-  // }
-
-  // static op = (opValue) => {
-  //   if (isNaN(this.#value[this.#value.length - 1])) {
-  //     return null
-  //   }
-
-  //   this.#value = this.#value.concat(opValue)
-  //   this.#output()
-  //   this.#isDot = false
-  // }
-
-  // static reset = () => {
-  //   this.#value = ''
-  //   this.#isDot = false
-  //   this.#output()
-  // }
-
-  // static result = () => {
-  //   this.#value = String(eval(this.#value))
-  //   this.#output()
-  // }
-
-  // static #save = () => {
-  //   window.localStorage.setItem(this.#NAME, this.#value)
-  // }
-
-  // static #load = () => {
-  //   this.#value =
-  //     window.localStorage.getItem(this.#NAME) || 0
-  // }
-
-  // static init = () => {
-  //   this.#load()
-  //   this.#output()
-  //   console.log('Calc is init')
-  // }
-}
+  }
 window.calc = Calc
